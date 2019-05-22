8eb521dc96b0207637967aa432e712a264da1b33
Farhan <fmshyder@gmail.com>
2019-05-22 00:08:55 -0400

    print_board updated with show_attack. update_under_attack func incomplete

--- NQueens_backtracking.py
+++ NQueens_backtracking.py
@@ -4,20 +4,29 @@ import numpy
 BOARD_SIZE = 10  # board will be a BOARD_SIZE*BOARD_SIZE matrix
 EMPTY = 0
 QUEEN = 1
+UNDER_ATTACK = 2    # if a position is marked UNDER_ATTACK, then it is not safe to play in that pos
 
 
 # create an empty BOARD_SIZE*BOARD_SIZE matrix
 def create_board():
     return numpy.zeros(shape=(BOARD_SIZE, BOARD_SIZE)).astype(int)
 
+def update_under_attack(board,row,col):
+    # update horizontal
+    for i,j in zip(range(0,row), range(row+1, BOARD_SIZE)):
+        board[i][j] = UNDER_ATTACK
+
+    return board
 
 # print board in more user friendly way
-def print_board(board):
+def print_board(board, show_attack=False):
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
             item = board[i][j]
             if item == QUEEN:
                 print("Q", end=" ")
+            elif item == UNDER_ATTACK and show_attack:
+                print("A", end=" ")
             else:
                 print("-", end=" ")
         print()
@@ -25,7 +34,9 @@ def print_board(board):
 
 def main():
     board = create_board()
-    print_board(board)
 
-# just a test line to check version control
+    board[5,5] = QUEEN
+    update_under_attack(board,5,5)
+    print_board(board, True)
+
 main()
