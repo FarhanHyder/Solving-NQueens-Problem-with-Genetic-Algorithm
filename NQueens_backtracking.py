bf8148ed7f9154a5cc8acdc58e0ff3ecd32bac29
Farhan <fmshyder@gmail.com>
2019-05-22 04:22:18 -0400

    rolling back to a previous version

--- NQueens_backtracking.py
+++ NQueens_backtracking.py
@@ -16,21 +16,45 @@ def create_board():
 # self-note: this function should be updated with several composite funcs for a more clean look
 # supplementary func for place_queen
 def update_under_attack(board, row, col):
+    # update horizontal
+    for i in it.chain(range(0, col), range(col + 1, BOARD_SIZE)):
+        board[row][i] = UNDER_ATTACK
+        if board[row][i] != QUEEN:
+            board[row][i] = UNDER_ATTACK
+
     # update vertical
-    for i in range(row + 1, BOARD_SIZE):
+    for i in it.chain(range(0, row), range(row + 1, BOARD_SIZE)):
+        board[i][col] = UNDER_ATTACK
         if board[i][col] != QUEEN:
             board[i][col] = UNDER_ATTACK
 
     # update diagonal: left-right
+    # upper-part
+        # upper-part
+    r_n = [i for i in range(row-1, -1, -1)]
+    c_n = [i for i in range(col-1, -1, -1)]
+    for i in range(min(len(r_n), len(c_n))):
+        board[r_n[i]][c_n[i]] = UNDER_ATTACK
+    # lower-part
+        if board[r_n[i]][c_n[i]] != QUEEN:
+            board[r_n[i]][c_n[i]] = UNDER_ATTACK
         # lower-part
     r_n = [i for i in range(row + 1, BOARD_SIZE)]
     c_n = [i for i in range(col + 1, BOARD_SIZE)]
     for i in range(min(len(r_n), len(c_n))):
+        board[r_n[i]][c_n[i]] = UNDER_ATTACK
         if board[r_n[i]][c_n[i]] != QUEEN:
             board[r_n[i]][c_n[i]] = UNDER_ATTACK
 
     # update diagonal: right-left
-        # lower-part
+    # upper-part
+        # upper-part
+    r_n = [i for i in range(row - 1, -1, -1)]
+    c_n = [i for i in range(col + 1, BOARD_SIZE)]
+    for i in range(min(len(r_n), len(c_n))):
+        if board[r_n[i]][c_n[i]] != QUEEN:
+            board[r_n[i]][c_n[i]] = UNDER_ATTACK
+    # lower-part
     r_n = [i for i in range(row + 1, BOARD_SIZE)]
     c_n = [i for i in range(col - 1, -1, -1)]
     for i in range(min(len(r_n), len(c_n))):
@@ -38,6 +62,7 @@ def update_under_attack(board, row, col):
             board[r_n[i]][c_n[i]] = UNDER_ATTACK
 
     return board
+
 
 # print board in more user friendly way
 def print_board(board, show_attack=False):
@@ -52,31 +77,30 @@ def print_board(board, show_attack=False):
                 print("-", end=" ")
         print()
 
+
 # pre: row, col must be in range
 def place_queen(board, row, col):
     board[row][col] = QUEEN
     update_under_attack(board,row,col)
 
-# post: returns the first available col in the given row
-#      : returns -1 if no col is available
-def get_first_available_square(board, row):
-    for i in range(BOARD_SIZE):
-        if board[row][i] == EMPTY:
-            return i
-    return -1
-
-
-def solve_nq(board, row):
-    place_queen(board, row, get_first_available_square(row))
-
 
 def main():
     board = create_board()
-    # place_queen(board,1,3)
-    place_queen(board,4,2)
-    print(get_first_available_square(board,4))
 
+    row = 3; col = 5
+    board[row, col] = QUEEN
+    update_under_attack(board, row, col)
+    place_queen(board,1,3)
+    place_queen(board,4,2)
     print_board(board, show_attack=True)
+
+
+    # try counting forward
+    r_n = [i for i in range(row+1, BOARD_SIZE)]
+    c_n = [i for i in range(col+1, BOARD_SIZE)]
+
+    for i in range(min(len(r_n), len(c_n))):
+        print(r_n[i], c_n[i])
 
 
 main()
