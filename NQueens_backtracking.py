93b0c5c285c3ad7564611b0a8c20d342334c53ba
Farhan <fmshyder@gmail.com>
2019-05-22 16:17:29 -0400

    solved bug for backtracking

--- NQueens_backtracking.py
+++ NQueens_backtracking.py
@@ -2,10 +2,13 @@ import numpy
 import itertools as it  # helps to iterate over a loop in two different ranges
 
 # global vars for the board
-BOARD_SIZE = 5  # board will be a BOARD_SIZE*BOARD_SIZE matrix
+BOARD_SIZE = 25  # board will be a BOARD_SIZE*BOARD_SIZE matrix
+# in order to try different try different chessboards, change BOARD_SIZE
+
 EMPTY = 0
 QUEEN = 1
-UNDER_ATTACK = 2    # used for testing purposes
+UNDER_ATTACK = 2  # used for testing purposes
+
 
 # create an empty BOARD_SIZE*BOARD_SIZE matrix
 def create_board():
@@ -14,38 +17,32 @@ def create_board():
 
 # returns true is a square is not attacked by any of the previously placed queen
 def is_square_available(board, row, col):
-
     # check vertical: up
     for i in it.chain(range(0, row)):
-        # if board[i][col] == QUEEN:
-        #     return False
-        if board[i][col] != QUEEN:
-            board[i][col] = UNDER_ATTACK
+        if board[i][col] == QUEEN:
+            return False
 
     # check diagonal: left-right
     # upper-part
     r_n = [i for i in range(row - 1, -1, -1)]
     c_n = [i for i in range(col - 1, -1, -1)]
     for i in range(min(len(r_n), len(c_n))):
-        # if board[r_n[i]][c_n[i]] == QUEEN:
-        #     return False
-        if board[r_n[i]][c_n[i]] != QUEEN:
-            board[r_n[i]][c_n[i]] = UNDER_ATTACK
+        if board[r_n[i]][c_n[i]] == QUEEN:
+            return False
 
     # check diagonal: right-left
     # upper-part
-    r_n = [i for i in range(row-1, -1, -1)]
-    c_n = [i for i in range(col+1, BOARD_SIZE)]
+    r_n = [i for i in range(row - 1, -1, -1)]
+    c_n = [i for i in range(col + 1, BOARD_SIZE)]
     for i in range(min(len(r_n), len(c_n))):
-        # if board[r_n[i]][c_n[i]] == QUEEN:
-        #     return False
-        if board[r_n[i]][c_n[i]] != QUEEN:
-            board[r_n[i]][c_n[i]] = UNDER_ATTACK
+        if board[r_n[i]][c_n[i]] == QUEEN:
+            return False
 
     return True
 
+
 # print board in more user friendly way
-def print_board(board, show_attack = False):
+def print_board(board, show_attack=False):
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
             item = board[i][j]
@@ -63,36 +60,40 @@ def place_queen(board, row, col):
     board[row][col] = QUEEN
 
 
-def solve_NQ(board, col):
-    if col >= BOARD_SIZE:
-        # means we were able to place a queen in the last col, that's a success
+# pre: row, col must be in range
+def remove_queen(board, row, col):
+    board[row][col] = EMPTY
+
+
+# pre: upon it's main call,
+# post: returns if a solution is available for a given board
+def is_solution_found(board, row=0):
+    if row >= BOARD_SIZE:
+        # means we were able to place a queen in the last row (board[BOARD_SIZE-1][some_col]), that's a success
         return True
 
     for i in range(0, BOARD_SIZE):
-        if is_square_available(board, i, col):
-            place_queen(board, i, col)
-            if solve_NQ(board, col + 1):
+        if is_square_available(board, row, i):
+            # square is available, place queen
+            place_queen(board, row, i)
+            # recursive call and place queen in the next row
+            # as there is constraint to placing queen in the same row
+            if is_solution_found(board, row + 1):
                 return True
 
-            board[i][col] = EMPTY
+            # placing the queen here didn't work. so remove it and backtrack to previous step.
+            remove_queen(board, row, i)
 
     return False
-
-
 
 
 def main():
     board = create_board()
 
-    row = 1; col =3
-    board[row][col] = QUEEN
-    is_square_available(board,row, col)
-
-    print_board(board, show_attack=True)
-    # print(square_is_available(board,6,1))
-
-    # if (solve_NQ(board, 0)):
-    #     print_board(board)
+    if is_solution_found(board):
+        print_board(board)
+    else:
+        print("Solution Not Found!")
 
 
 main()
