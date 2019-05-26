5913dbb5a83aed60561f7285f0caa381639e8c35
Farhan <fmshyder@gmail.com>
2019-05-26 16:22:51 -0400

    test_randomize_chromosome() success

--- nqueens_ga.py
+++ nqueens_ga.py
@@ -1,15 +1,42 @@
 import itertools as it  # helps to iterate over a loop in two different ranges
 import numpy as np
-
-
+import random
 
 START_POPULATION = 70
-BOARD_SIZE = 5
+BOARD_SIZE = 6
 
 class Chromosome:
-    def __init__(self,c_data, fitness_score):
-        self.c_data = c_data
-        self.fitness_score = fitness_score
+    def __init__(self,c_data = 0, fitness_score = 0, randomize = False):
+        if randomize:
+            # default constructor
+            chromo = self.randomize_chromosome()
+            self.c_data = chromo.c_data
+            self.fitness_score = chromo.fitness_score
+        else:
+            # constructor with params
+            self.c_data = c_data
+            self.fitness_score = fitness_score
+
+    # post: returns a randomized Chromosome
+        # that might have a few nq violations
+    def randomize_chromosome(self):
+        nq = N_queens()
+        c_data = nq.create_empty_board()
+        # randomly place a queen in each row
+        for i in range(BOARD_SIZE):
+            col = random.randint(0, BOARD_SIZE - 1)  # 0 <= col < BOARD_SIZE
+            nq.place_queen(c_data, i, col)
+
+        fitness_score = nq.get_fitness_score(c_data)
+
+        return Chromosome(c_data, fitness_score)
+
+    def print_chromosome(self):
+        nq = N_queens()
+        print("Chromosome data:")
+        nq.print_board(self.c_data)
+        print("Chromosome fitness score: ", round(self.fitness_score, 2))
+
 
 class N_queens:
     QUEEN = 1
@@ -64,6 +91,27 @@ class N_queens:
 
         return violations
 
+    # returns the col index of the queen in the given row, returns -1 if queen not found
+    def get_queen_col_index(self, board, row):
+        for i in range(BOARD_SIZE):
+            if board[row][i] == self.QUEEN:
+                return i
+        return -1
+
+    # post: returns a number between 0-100
+    def get_fitness_score(self, board):
+        violations = 0
+        for i in range(BOARD_SIZE):
+            # find the queen in this row
+            col = self.get_queen_col_index(board, i)
+            # check violations for this queen
+            violations += self.check_violation(board, i, col)
+
+        # round fitness between 0,100
+        violations = max(0, min(violations, BOARD_SIZE))  # clip violations between 0-100
+        fitness = float(((BOARD_SIZE - violations) * 100) / BOARD_SIZE)
+        return fitness
+
 class Epoch:
     def __init__(self):
         self.data = []
@@ -74,6 +122,12 @@ class Epoch:
     def init_first_epoch(self):
         pass
 
+
+
+def test_randomize_chromosome():
+    chromo = Chromosome(randomize=True)
+    chromo.print_chromosome()
+test_randomize_chromosome()
 
 
 def test_nq():
@@ -88,5 +142,5 @@ def test_nq():
     print("Test: removing stuff")
     nq.remove_queen(board, 3,1)
     nq.print_board(board)
-test_nq()
+# test_nq()
 
