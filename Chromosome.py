6f66dc3afe8a972686dbaced9c18b441403d4778
Farhan <fmshyder@gmail.com>
2019-08-05 00:34:36 -0400

    added: selection probab

--- Chromosome.py
+++ Chromosome.py
@@ -1,29 +1,34 @@
-import NQueens
 from NQueens import *
+from random import randint, uniform
 
-# vars for n_queens
-EMPTY = 0
-QUEEN = 1
+
 
 class Chromosome:
     nQueens = NQueens()
     SIZE = BOARD_SIZE
-    body = ""
-    fitness = 0.0
+    body = ""                       # n*n matrix
+    fitness = 0.0                   # [0.0, 100.0]
+    selection_probability = 0       # [0.0, 1]
 
-    def __init__(self, size = BOARD_SIZE):
+    def __init__(self, size = BOARD_SIZE, randomize = False):
         self.SIZE = size
         self.body = self.nQueens.create_empty_board(self.SIZE)
+
+        if randomize:
+            # randomly place a queen in each row
+            for i in range(self.SIZE):
+                col = randint(0, self.SIZE - 1)  # 0 <= col < BOARD_SIZE
+                self.nQueens.place_queen(self.body, i, col)
+
         self.fitness = self.nQueens.get_fitness(self.body,self.SIZE)
 
     # print a 2D array in a more user friendly way
     def print_chromosome(self):
-        print_board(self.body, self.SIZE)
-        print(self.fitness)
+        print_board(self.body, self.SIZE, msg="[ Body ]")
+        print("fitness : ",self.fitness,"%",sep="")
 
-
-chromo = Chromosome(6)
-chromo.print_chromosome()
-
+#
+# chromo = Chromosome(6, randomize=True)
+# chromo.print_chromosome()
 
 
