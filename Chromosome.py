34990cdd5f37e69fa3a7d00875a6c6b94ac10d5b
Farhan <fmshyder@gmail.com>
2019-08-06 03:48:22 -0400

    added: update_fitness()

--- Chromosome.py
+++ Chromosome.py
@@ -6,11 +6,11 @@ from random import randint, uniform
 class Chromosome:
     nQueens = NQueens()
     SIZE = BOARD_SIZE
-    body = ""                       # n*n matrix
+    body = []                      # n*n matrix
     fitness = 0.0                   # [0.0, 100.0]
     selection_probability = 0       # [0.0, 1]
 
-    def __init__(self, size = BOARD_SIZE, randomize = False):
+    def __init__(self, size=BOARD_SIZE, randomize=False):
         self.SIZE = size
         self.body = self.nQueens.create_empty_board(self.SIZE)
 
@@ -20,7 +20,10 @@ class Chromosome:
                 col = randint(0, self.SIZE - 1)  # 0 <= col < BOARD_SIZE
                 self.nQueens.place_queen(self.body, i, col)
 
-        self.fitness = self.nQueens.get_fitness(self.body,self.SIZE)
+        self.fitness = self.nQueens.get_fitness(self.body, self.SIZE)
+
+    def update_fitness(self):
+        self.fitness = self.nQueens.get_fitness(self.body, self.SIZE)
 
     # print a 2D array in a more user friendly way
     def print_chromosome(self):
